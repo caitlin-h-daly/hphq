e3422f8d71a37c03236d100b30a09cbb4fc2888e
Caitlin Daly <c6daly@uwaterloo.ca>
2025-05-22 22:14:25 -0400

    Slight tidy of documentation for prep_data().

--- R/prep_data.R
+++ R/prep_data.R
@@ -1,41 +1,49 @@
-#' Data preparation
+#' Prepare data to determine credible hierarchies
 #'
 #' @description
-#' Produces a list of inputs to be used altogether in get_hierarchies(), or individually in consecutive(), get_posets(), and singular_treatment()
+#' `prep_data()` produces a list of inputs to be used altogether in
+#' `get_hierarchies()`, or individually in `consecutive()`, `get_cred_hier()`,
+#' and `singular_treatment()`.
 #'
-#' @param df A data frame where the column headers are treatment names and row headers are the iteration number. Each row displays each treatment’s sampled relative effect for that iteration
-#' @param reference A string of the reference treatment's name
-#' @param largerBetter Boolean value where True indicates larger values are better, and False otherwise
+#' @param effects_matrix a matrix where the column headers are treatment names
+#'   and each row displays each treatment’s sampled relative effect for that
+#'   iteration.
+#' @param reference a character string of the reference treatment's name.
+#' @param largerBetter a logical value indicating whether larger relative
+#'   effects are better (TRUE) or not (FALSE).
 #'
-#' @return \code{hierarchy.matrix} - A matrix where column headers are ranks and each row displays the treatments assigned to each rank for that iteration
-#' @return \code{effects.matrix} - A matrix where the column headers are treatment names and each row displays each treatment’s sampled relative effect for that iteration
+#' @return \code{hierarchy.matrix} - A matrix where column headers are ranks and
+#' each row displays the treatments assigned to each rank for that iteration.
+#' @return \code{effects.matrix} - A matrix where the column headers are
+#' treatment names and each row displays each treatment’s sampled relative
+#' effect for that iteration.
 #' @return \code{ranking.df} - A data frame of each treatment's ranks and associated frequencies
 #' @importFrom stats reshape
 #' @export
 #'
 #' @examples
-#' prep_data(df,"waitlist",TRUE)
-prep_data <- function(df,reference,largerBetter) {
-  treatments<-colnames(df)
+#' prep_data(df, "waitlist", TRUE)
+prep_data <- function(df, reference, largerBetter) {
+  treatments < -colnames(df)
   if (reference %in% treatments) {
     x <- df
   } else {
     warning("Relative effects for reference treatment not detected. A vector of 0's for the reference treatment has been added, assuming the input is an MCMC sample from a Bayesian framework. Ensure sampled relative effects are on the additive scale such that the null effect is 0.")
-    x<-data.frame(0,df)
+    x <- data.frame(0,df)
     colnames(x)[1] <- reference
-    treatments <-colnames(x)
+    treatments <- colnames(x)
   }
   for (trt in treatments) {
-    if (str_detect(trt,",")) {
+    if (str_detect(trt, ",")) {
       stop("Comma detected in treatment name. Please remove all commas from treatment names.")
     }
   }
 
-  n.treatments<-length(treatments)
+  n_trt <- length(treatments)
   value_matrix <- as.matrix(x)
   x2 <- x
-  n.iterations <- nrow(x2)
-  x2$iteration <- seq_len(n.iterations)
+  n_iter <- nrow(x2)
+  x2$iteration <- seq_len(n_iter)
   x3 <- reshape(x2,
                 varying = names(x2)[-ncol(x2)],
                 v.names = "value",
@@ -54,27 +62,27 @@ prep_data <- function(df,reference,largerBetter) {
   strs <- sapply(split(x3_sorted$trt, x3_sorted$iteration), function(x) paste(x, collapse = ","))
   tbl<- t(sapply(strs, str_split_1, ","))
 
-  inputs <- vector("list",length=3)
+  inputs <- vector("list", length=3)
   inputs[[1]] <- tbl
   inputs[[2]] <- value_matrix
 
   # prep for single treatment
-  table_list<-list()
+  table_list <- list()
 
-  for (i in seq(1:n.treatments)) {
-    temp_table <- table(tbl[,i])/n.iterations
-    temp_df <- data.frame(names(temp_table),i,as.numeric(temp_table))
+  for (i in seq(1:n_trt)) {
+    temp_table <- table(tbl[,i]) / n_iter
+    temp_df <- data.frame(names(temp_table), i, as.numeric(temp_table))
     colnames(temp_df) <- c("Treatment","Rank","Freq")
-    if (nrow(temp_df) != n.treatments) {
+    if (nrow(temp_df) != n_trt) {
       x <- temp_df$Treatment
       missing <- setdiff(treatments, x)
-      vec <- data.frame(missing,i,0)
-      colnames(vec) <- c("Treatment","Rank","Freq")
-      temp_df <- rbind(temp_df,vec)
+      vec <- data.frame(missing, i, 0)
+      colnames(vec) <- c("Treatment", "Rank", "Freq")
+      temp_df <- rbind(temp_df, vec)
     }
     table_list[[i]] <- temp_df
   }
-  inputs[[3]]<-do.call(rbind,table_list)
-  names(inputs)<-c("hierarchy.matrix","effects.matrix","ranking.df")
+  inputs[[3]] <- do.call(rbind, table_list)
+  names(inputs) <- c("hierarchy_matrix", "effects_matrix", "ranking_df")
   return (inputs)
 }
